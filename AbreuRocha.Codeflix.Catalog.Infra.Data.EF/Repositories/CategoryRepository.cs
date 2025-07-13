560c7048c09304409dee99083cf755706b49a747
jlferrazabreu <jlferrazabreu@gmail.com>
2025-07-13 08:43:12 -0300

    feat(Repositories): Criação do projeto AbreuRocha.Codeflix.Catalog.Infra.Data.EF do Repositorio e suas configurações e implementando o Insert na base

---
+++ AbreuRocha.Codeflix.Catalog.Infra.Data.EF/Repositories/CategoryRepository.cs
@@ -0,0 +1,44 @@
+﻿using AbreuRocha.Codeflix.Catalog.Domain.Entity;
+using AbreuRocha.Codeflix.Catalog.Domain.Repository;
+using AbreuRocha.Codeflix.Catalog.Domain.SeedWork.SearchableRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbreuRocha.Codeflix.Catalog.Infra.Data.EF.Repositories;
+public class CategoryRepository
+    : ICategoryRepository
+{
+    private readonly CodeflixCatalogDbContext _context;
+    private DbSet<Category> _categories 
+        => _context.Set<Category>();
+
+    public CategoryRepository(CodeflixCatalogDbContext context)
+        => _context = context;
+
+
+    public async Task Insert(
+        Category aggergate, 
+        CancellationToken cancellationToken
+    )
+    {
+        await _categories.AddAsync(aggergate, cancellationToken);
+    }
+    public Task Delete(Category aggergate, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Category> Get(Guid Id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<SearchOutput<Category>> Search(SearchInput input, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Update(Category aggergate, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AbreuRocha.Codeflix.Catalog.IntegrationTests/InfraDataEF/Repositories/CategoryRepository/CategoryRepositoryTest.cs
@@ -0,0 +1,35 @@
+﻿using AbreuRocha.Codeflix.Catalog.Infra.Data.EF;
+using Repository = AbreuRocha.Codeflix.Catalog.Infra.Data.EF.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using FluentAssertions;
+
+namespace AbreuRocha.Codeflix.Catalog.IntegrationTests.InfraDataEF.Repositories.CategoryRepository;
+
+[Collection(nameof(CategoryRepositoryTestFixture))]
+public class CategoryRepositoryTest
+{
+    private readonly CategoryRepositoryTestFixture _fixture;
+
+    public CategoryRepositoryTest(CategoryRepositoryTestFixture fixture)
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(Insert))]
+    [Trait("Integration/Infra.Data", "CategoryRepository - Repositories")]
+    public async Task Insert()
+    {
+        CodeflixCatalogDbContext dbContext = _fixture.CreateDbContext();
+        var examploCategory = _fixture.GetExampleCategory();
+        var categoryRepository = new Repository.CategoryRepository(dbContext);
+
+        await categoryRepository.Insert(examploCategory, CancellationToken.None);
+        await dbContext.SaveChangesAsync(CancellationToken.None);
+
+        var dbCategory = await dbContext.Categories.FindAsync(examploCategory.Id);
+        dbCategory.Should().NotBeNull();
+        dbCategory!.Name.Should().Be(examploCategory.Name);
+        dbCategory.Description.Should().Be(examploCategory.Description);
+        dbCategory.IsActive.Should().Be(examploCategory.IsActive);
+        dbCategory.CreatedAt.Should().Be(examploCategory.CreatedAt);
+    }
+}

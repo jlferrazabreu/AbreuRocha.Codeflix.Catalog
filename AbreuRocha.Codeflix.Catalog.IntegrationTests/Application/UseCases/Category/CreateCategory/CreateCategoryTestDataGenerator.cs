0a911e24502bdd873d1a38d9719b94ad7e68b2c3
jlferrazabreu <jlferrazabreu@gmail.com>
2025-07-14 21:51:59 -0300

    feat(integrationCreateCategory): Implementação do teste de integração do Create categoria

---
+++ AbreuRocha.Codeflix.Catalog.IntegrationTests/Application/UseCases/Category/CreateCategory/CreateCategoryTestDataGenerator.cs
@@ -0,0 +1,43 @@
+﻿namespace AbreuRocha.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.CreateCategory;
+public class CreateCategoryTestDataGenerator
+{
+    public static IEnumerable<object[]> GetInvalidInputs(int times = 12)
+    {
+        var fixture = new CreateCategoryTestFixture();
+        var invalidInputslist = new List<object[]>();
+        var totalInvalidCases = 4;
+
+        for (int index = 0; index < times; index++)
+        {
+            switch (index % totalInvalidCases)
+            {
+                case 0:
+                    invalidInputslist.Add(new object[] {
+                        fixture.GetInvalidInputShortName(),
+                        "Name should be at least 3 characters long"
+                    });
+                    break;
+                case 1:
+                    invalidInputslist.Add(new object[] {
+                        fixture.GetInvalidInputTooLongName(),
+                        "Name should be less or equal 255 characters long"
+                    });
+                    break;
+                case 2:
+                    invalidInputslist.Add(new object[] {
+                        fixture.GetInvalidInputDescriptionNull(),
+                        "Description should not be null"
+                    });
+                    break;
+                case 3:
+                    invalidInputslist.Add(new object[] {
+                        fixture.GetInvalidInputTooLongDescription(),
+                        "Description should be less or equal 10000 characters long"
+                    });
+                    break;
+            }
+        }
+
+        return invalidInputslist;
+    }
+}
---
+++ AbreuRocha.Codeflix.Catalog.IntegrationTests/Application/UseCases/Category/CreateCategory/CreateCategoryTestFixture.cs
@@ -0,0 +1,59 @@
+﻿using AbreuRocha.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+using AbreuRocha.Codeflix.Catalog.IntegrationTests.Application.UseCases.Common;
+using Xunit;
+
+namespace AbreuRocha.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.CreateCategory;
+[CollectionDefinition(nameof(CreateCategoryTestFixture))]
+public class CreateCategoryTestFixtureCollection
+    : ICollectionFixture<CreateCategoryTestFixture>
+{ }
+public class CreateCategoryTestFixture
+    : CategoryUseCasesBaseFixture
+{ 
+    public CreateCategoryInput GetInput()
+    {
+        var category = GetExampleCategory();
+        return new CreateCategoryInput(
+            category.Name,
+            category.Description,
+            category.IsActive
+        );
+    }
+
+    public CreateCategoryInput GetInvalidInputShortName()
+    {
+        var invalidInputShortName = GetInput();
+        invalidInputShortName.Name =
+            invalidInputShortName.Name.Substring(0, 2);
+        return invalidInputShortName;
+    }
+
+    public CreateCategoryInput GetInvalidInputTooLongName()
+    {
+        var invalidInputTooLongName = GetInput();
+        invalidInputTooLongName.Name =
+            Faker.Commerce.ProductName();
+        while (invalidInputTooLongName.Name.Length <= 255)
+            invalidInputTooLongName.Name +=
+                Faker.Commerce.ProductName();
+        return invalidInputTooLongName;
+    }
+
+    public CreateCategoryInput GetInvalidInputDescriptionNull()
+    {
+        var invalidInputDescriptionNull = GetInput();
+        invalidInputDescriptionNull.Description = null!;
+        return invalidInputDescriptionNull;
+    }
+
+    public CreateCategoryInput GetInvalidInputTooLongDescription()
+    {
+        var invalidInputTooLongDescription = GetInput();
+        invalidInputTooLongDescription.Description =
+            Faker.Commerce.ProductDescription();
+        while (invalidInputTooLongDescription.Description.Length <= 10_000)
+            invalidInputTooLongDescription.Description +=
+                Faker.Commerce.ProductDescription();
+        return invalidInputTooLongDescription;
+    }
+}
